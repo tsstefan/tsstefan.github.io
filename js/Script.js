dc3c2dced90ce952c27481f51988c82324e6daa0
tsstefan <stefan.tschimben@colorado.edu>
2016-08-09 10:22:59 +0200

    update

--- js/Script.js
+++ js/Script.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
 //  $("#tricked").hide();
   $("#button1").click(function(){
     $("#gitHub").fadeOut(3000);
+    $("#background").animate({
+      height:'75%',
+      width:'75%'
+      }, "slow");
   });
   $("#button2").mouseenter(function(){
     $("#gitHub").fadeIn(3000);
