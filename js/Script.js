5e86d9acfb188b08a9ed3b989c57b33f9be9c8bc
tsstefan <stefan.tschimben@colorado.edu>
2016-08-09 09:40:27 +0200

    Error fixing

--- js/Script.js
+++ js/Script.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
-  $(".fadeOut").click(function(){
+  $("#button1").click(function(){
     $("#gitHub").fadeOut();
+  });
+  $("#button2").mouseenter(function(){
+    $("#gitHub").fadeIn();
   });
 });
 
 //(HTML Object).action(do);
+// .dblclick .mouseenter . mouseleave
