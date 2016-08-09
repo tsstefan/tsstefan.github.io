7f9d6e923eec637fd19d71fa4f9e03728cf3e9b9
tsstefan <stefan.tschimben@colorado.edu>
2016-08-09 10:45:42 +0200

    File Updates

--- js/Script.js
+++ js/Script.js
@@ -1,33 +1,4 @@
 $(document).ready(function(){
-//  $("#tricked").hide();
-  $("#button1").click(function(){
-    $("#gitHub").fadeOut(3000);
-    $("#background").animate({
-      height:'75%',
-      width:'75%'
-      }, "slow");
-  });
-  $("#button2").mouseenter(function(){
-    $("#gitHub").fadeIn(3000);
-    $("#tricked").css("color", "black");
-    $("#background").animate({
-      height:'50%',
-      width:'50%'
-      }, "slow");
-  });
-  $("#tButton1").click(function(){
-    $("#gitHub").toggle();
-    $("#background").animate({left: '0px'}, "slow");
-  });
+  var food = 'Pizza';
+  alert(food);
 });
-
-//(HTML Object).action(do);
-// .dblclick .mouseenter . mouseleave
-// .hide .show
-// "slow" "fast"
-/* incremental growth/shrink:
-    $("#background").animate({
-      height:'+=50%',
-      width:'-=50%'
-      }, "slow");
-*/
