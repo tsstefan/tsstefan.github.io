9cd2ffa4007f33364817302e8343e3fe244d1c6a
tsstefan <stefan.tschimben@colorado.edu>
2016-08-10 12:38:36 +0200

    js update

--- js/youtube.js
+++ js/youtube.js
@@ -1,3 +1,31 @@
+//Searchbar Handler
+$(function(){
+    var searchField = $('#query');
+    var icon = $('#searchButton');
+    
+    //Focus Handler
+    $(searchField).on('focus', function(){
+        $(this).animate({
+            width: '100%'    
+        }, 300);
+        $(icon).animate({
+            right: '10px'
+        }, 300);
+    });
+    
+    //Blur Event Handler
+    $(searchField).on('blur', function(){
+        if(searchField.val === ''){
+            $(searchField).animate({
+                width: '45%'
+            }, 300, function (){});
+            $(icon).animate({
+                right: '355px'
+            }, 300, function (){});
+        }
+    });
+)}
+
 $(document).ready(function(){
 
 });
