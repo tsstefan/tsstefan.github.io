0573e354f5491de53cf4dffb28c8e07b37419805
tsstefan <stefan.tschimben@colorado.edu>
2016-08-10 21:43:51 +0200

    js t&e

--- js/youtube.js
+++ js/youtube.js
@@ -169,7 +169,7 @@ function getOutput(item){
     '<img src="'+ thumb +'">' +
     '</div>' +
     '<div class="list-right">' +
-    '<h3><a id="print-button" href="https://www.youtube.com/embed/'+videoId+'">'+title+'</a></h3>' +
+    '<h3><a id="print-button">'+title+'</a></h3>' +
     '<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
     '<p>'+description+'</p>' +
     '</div>' +
