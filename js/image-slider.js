0440d18a5e63a31097cba6deaf423398571facd5
tsstefan <stefan.tschimben@colorado.edu>
2016-08-11 14:00:14 +0200

    js update

---
+++ js/image-slider.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+   var totalWidth = 0;
+   var positions = new Array();
+   
+   $('#slides, .slide').each(function(){
+        $positions[i] = totalwidth;
+        totalWidth += $(this).width;
+        
+        if(!$(this).width()){
+            alert('Please add a width to your images');
+            return false;
+        }
+   });
+   //set width
+   $('slides').width(totalWidth);
+   
+   $('#menu ul li a').click(function(e, keepScroll){
+        //remove active class, add inactive
+        $('li.photo').removeClass('active').addClass('inactive');
+        //add active class to parent
+        $(this).parent().addClass('active');
+        
+        var pos = $(this).parent().prevAll('.photo').length;
+        
+        $('#slides').stop().animate({marginLeft:-positions[pos]+'px'}, 450);
+        
+        //prevent default
+        e.preventDefault();
+        //stopping autoscroll
+        if(!autoScroll) clearInterval(itvl);
+    });
+   
+   //Make first image active
+   $('#menu ul li.photo:first').addClass('active').siblings().addClass('inactive');
+   
+   //autoScroll
+   var current = 1;
+   function autoScroll(){
+        if(current == -1){
+            return false;
+        }
+        //eq reduces matches at specified index to 1
+        $('#menu ul li a').eq(current%$('#menu ul li a').length).trigger('click', [true]);
+        current ++;
+   }
+   
+   //dureation for autoscroll
+   var duration = 10;
+   var itvl = setInterval(function(){autoScroll()}, duration*1000);
+});
