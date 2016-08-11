0e36de06d2cfe5ed1fcd603e81561a690e1fd1a8
tsstefan <stefan.tschimben@colorado.edu>
2016-08-11 09:24:10 +0200

    New files accordian

--- js/accordian.js
+++ js/accordian.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
+//jQuery UI
+var acition="click";
+var speed="500";
 
+$(document).ready(function(){
+    $('li.q').on(action, function(){
+        //toggle a slide for the next element after the clicked one and slide up all other siblings
+        $(this).next().slideToggle(speed).siblings('li.a').slideUp();
+        var img = $(this).children('img');
+        //Remove the rotate class for all images except the active one
+        $('img').not(img).removeClass('rotate');
+        //toggle rotate Class
+        img.toggleClass('rotate');
+        });
 });
